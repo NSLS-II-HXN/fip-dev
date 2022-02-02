457b70a83b8de4b8d2b1a7add366a366bda06ba5
Dmitri Gavrilov <gavrilov.dvs@gmail.com>
2022-02-02 10:54:15 -0500

    ENH: initial set of startup files

---
+++ startup/11-optics.py
@@ -0,0 +1,6 @@
+print(f"Loading {__file__}...")
+
+from ophyd import EpicsMotor, EpicsSignal, EpicsSignalRO
+from ophyd import Device, Signal
+from ophyd import Component as Cpt
+from ophyd import PVPositionerPC
