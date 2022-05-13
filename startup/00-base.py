2dbba2f8cbb03b356e94fcfdf99f0b6dc67a485b
Dmitri Gavrilov <gavrilov.dvs@gmail.com>
2022-05-13 12:48:46 -0400

    Modified plans

--- startup/00-base.py
+++ startup/00-base.py
@@ -18,7 +18,7 @@ class SRXPrompt(Prompts):
         ]
 
 ip = get_ipython()
-nslsii.configure_base(ip.user_ns, "temp")
+nslsii.configure_base(ip.user_ns, "hxn")
 ip.log.setLevel('WARNING')
 
 # nslsii.configure_olog(ip.user_ns)
